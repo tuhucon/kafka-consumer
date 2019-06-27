3abf8672e3d86ff9667ddb14b5e1a49db39f8f8a
LAP01171 <lap01171@LAP01171s-MacBook-Pro.local>
2019-06-27 08:40:15 +0700

    consumer multiple topic + partition asignor -> sticky, after 1 minutes consumer change partition - still don't know where is config

--- src/main/java/com/example/kafkaconsumer/KafkaConsumerApplication.java
+++ src/main/java/com/example/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,6 +1,7 @@
 package com.example.kafkaconsumer;
 
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.internals.PartitionAssignor;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.Cluster;
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.InetSocketAddress;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,6 +22,8 @@ import java.util.Properties;
 
 @SpringBootApplication
 public class KafkaConsumerApplication implements CommandLineRunner {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaConsumerApplication.class, args);
@@ -31,24 +35,46 @@ public class KafkaConsumerApplication implements CommandLineRunner {
         pros.setProperty("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
         pros.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         pros.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        pros.setProperty("group.id", "hello");
+        pros.setProperty("group.id", "collection1");
         pros.setProperty("enable.auto.commit", "false");
-        pros.setProperty("max.poll.records", "1");
+        pros.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.StickyAssignor");
+//        pros.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.RangeAssignor");
+        pros.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        pros.setProperty("max.poll.records", "1");
 
-        Consumer<String, String> consumer = new KafkaConsumer(pros);
-//        List<TopicPartition> topicPartitions = new LinkedList<>();
-//        for (int i = 0; i < 3; i++) {
-//            topicPartitions.add(new TopicPartition("hello", i));
-//        }
-//        consumer.seekToBeginning(topicPartitions);
-        consumer.subscribe(Arrays.asList("hello"));
+        KafkaConsumer<String, String> consumer = new KafkaConsumer(pros);
+        consumer.subscribe(Arrays.asList("hello", "tuhucon"));
 
+        String topic = "";
+        String partition = "";
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            System.out.println("record count: " + records.count());
+            int hello_count = 0;
+            int tuhucon_count = 0;
             for (ConsumerRecord<String, String> record: records) {
-                System.out.println("key = " + record.key() + ", value = " + record.value() + " at offset = " + record.offset() + " in partition = " + record.partition());
+//                System.out.println("key = " + record.key() + ", value = " + record.value() + " at offset = " + record.offset()
+//                                    + " in partition = " + record.partition() + " of topic = " + record.topic());
+                if (record.topic().equals(topic) == false) {
+                    System.out.println(LocalDateTime.now());
+                    topic = record.topic();
+                    partition = record.partition() + "";
+                } else if (partition.equals(record.partition() + "") == false) {
+                    System.out.println(LocalDateTime.now());
+                    partition = record.partition() + "";
+                }
+
+
+                if (record.topic().equals("hello")) {
+                    hello_count++;
+                } else {
+                    tuhucon_count++;
+                }
             }
             consumer.commitSync();
+            System.out.println("topic = " + topic + " parition = " + partition);
+            System.out.println("hello count: " + hello_count);
+            System.out.println("tuhucon count: " + tuhucon_count);
             Thread.sleep(1_000L);
         }
 
